529a3a76d17626a4736230fea2cc46bdefbb52b7
mrok <stwarog@gmail.com>
2016-11-28 00:12:57 +0100

    It is faster

--- ClumpFindingProblem/BetterClumpFindingProblem.py
+++ ClumpFindingProblem/BetterClumpFindingProblem.py
@@ -2,53 +2,9 @@ import sys
 from collections import Counter
 import os
 
-# enumerating list is too slow
 # It is better(faster) because frequency array is not calculated for each window,
 # but it is constructed from previous array by subtracting first element and adding
 # last element of the next window
-
-
-def digit_to_symbol(number):
-    mapping = ['A', 'C', 'G', 'T']
-
-    return mapping[number]
-
-
-def number_to_pattern(number, k):
-    if k == 1:
-        return digit_to_symbol(number)
-
-    quotient = number // 4
-    remainder = number % 4
-
-    symbol = digit_to_symbol(remainder)
-    part_pattern = number_to_pattern(quotient, k - 1)
-
-    return part_pattern + symbol
-
-
-def symbol_to_number(symbol):
-    mapping = {
-        'A': 0,
-        'C': 1,
-        'G': 2,
-        'T': 3
-    }
-
-    return mapping.get(symbol)
-
-
-def pattern_to_number(pattern):
-    if len(pattern) == 1:
-        return symbol_to_number(pattern)
-
-    last_symbol = pattern[-1:]
-    new_pattern = pattern[:-1]
-
-    result = 4 * pattern_to_number(new_pattern) + symbol_to_number(last_symbol)
-
-    return result
-
 
 def compute_frequencies(genome, k):
     frequencies = {}
@@ -65,11 +21,8 @@ def find_clumps(genome, k, L, t):
     initial_frequencies = compute_frequencies(initial_window, k)
 
     final_result = [key for key, value in initial_frequencies.items() if value >= t]
-
     frequencies = initial_frequencies
-
-    genome_length = len(genome)
-    for i in range(1, genome_length - L + 1):
+    for i in range(1, len(genome) - L + 1):
         if (i % 100000 == 1):
             print(i)
         beggining = genome[i - 1:i - 1 + k]
@@ -83,7 +36,6 @@ def find_clumps(genome, k, L, t):
         final_result.extend([key for key, value in frequencies.items() if value >= t])
 
     final_result = list(set(final_result))
-
     return final_result
 
 if __name__ == "__main__":
@@ -102,3 +54,4 @@ if __name__ == "__main__":
     params.extend(clump_params)
     result = find_clumps(params[0], int(params[1]), int(params[2]), int(params[3]))
     print(' '.join(result))
+    print(len(result))    
